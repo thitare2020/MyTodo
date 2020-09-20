728b25113dfa5f519f2f45b3b95cda64b150d2af
thitare2020 <71217509+thitare2020@users.noreply.github.com>
2020-09-21 03:11:49 +0700

    loading

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,18 @@ class MyApp extends StatelessWidget {
         // Once complete, show your application
         if (snapshot.connectionState == ConnectionState.done) {
           return MaterialApp(
+<<<<<<< Updated upstream
 
+=======
+            debugShowCheckedModeBanner: false,
+>>>>>>> Stashed changes
             home: mainPage(),
           );
         }
 
         // Otherwise, show something whilst waiting for initialization to complete
         return MaterialApp(
-          home: LoadingPages(),
-        );
+            home: LoadingPages(), debugShowCheckedModeBanner: false);
       },
     );
   }
@@ -48,12 +51,21 @@ class mainPage extends StatefulWidget {
 }
 
 class _mainPageState extends State<mainPage> {
+<<<<<<< Updated upstream
   final collection = FirebaseFirestore.instance.collection('myTodo');
   final textController = TextEditingController();
+=======
+  final _collection = FirebaseFirestore.instance.collection('myTodo');
+  final _textController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+>>>>>>> Stashed changes
   void _showDialog() {
     showDialog(
         context: context,
         builder: (BuildContext context) {
+<<<<<<< Updated upstream
           return AlertDialog(
             content: Padding(
                 padding: EdgeInsets.all(5.0),
@@ -91,6 +103,9 @@ class _mainPageState extends State<mainPage> {
               )
             ],
           );
+=======
+          return _myDialog();
+>>>>>>> Stashed changes
         });
   }
 
@@ -111,6 +126,7 @@ class _mainPageState extends State<mainPage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+        key: _scaffoldKey,
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: _showDialog,
@@ -118,6 +134,7 @@ class _mainPageState extends State<mainPage> {
         appBar: AppBar(
           title: Text('Todo-List App'),
         ),
+<<<<<<< Updated upstream
         body: StreamBuilder<QuerySnapshot>(
             stream: collection.snapshots(),
             builder: (BuildContext context,AsyncSnapshot snapshot) {
@@ -165,18 +182,130 @@ class _mainPageState extends State<mainPage> {
                 );
               }
             }));
+=======
+        body: _myTodos());
+>>>>>>> Stashed changes
   }
 
   Widget _showSnackBar(DocumentSnapshot document) {
     String title = document.data()['Title'];
     return SnackBar(
+<<<<<<< Updated upstream
+=======
+        duration: Duration(milliseconds: 2000),
+>>>>>>> Stashed changes
         content: Text('$title is Complete'),
         action: SnackBarAction(
           label: 'Undo',
           onPressed: () {
             // Some code to undo the change.
+<<<<<<< Updated upstream
             collection.doc(document.id).set({'Title': title, 'isCheck': false});
+=======
+            _collection
+                .doc(document.id)
+                .set({'Title': title, 'isCheck': false});
+>>>>>>> Stashed changes
           },
         ));
+  }
+
+  Widget _myTodos() {
+    return StreamBuilder<QuerySnapshot>(
+        stream: _collection.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            return _myList(snapshot);
+          } else {
+            return Align(
+              alignment: FractionalOffset.center,
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
+  }
+
+  Widget _myDialog() {
+    return AlertDialog(
+      content: Padding(
+          padding: EdgeInsets.all(5.0),
+          child: Form(
+              key: _formKey,
+              child: TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  controller: _textController,
+                  decoration: InputDecoration(
+                    hintText: ('please input here'),
+                  )))),
+      elevation: 24.0,
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Cancel'),
+        ),
+        FlatButton(
+          color: Colors.blue,
+          textColor: Colors.white,
+          disabledColor: Colors.grey,
+          disabledTextColor: Colors.black,
+          padding: EdgeInsets.all(8.0),
+          splashColor: Colors.blueAccent,
+          onPressed: () {
+            _insertion();
+            if (_formKey.currentState.validate()) {
+              Navigator.of(context).pop();
+              _textController.clear();
+            }
+          },
+          child: Text(
+            "Insert",
+            style: TextStyle(fontSize: 15.0),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _myList(AsyncSnapshot<QuerySnapshot> snapshot) {
+    return ListView(
+      shrinkWrap: true,
+      padding: const EdgeInsets.all(10),
+      children: snapshot.data.docs.map((DocumentSnapshot document) {
+        if (document.data()['isCheck'] == true) {
+          Timer(Duration(milliseconds: 1000), () {
+            _scaffoldKey.currentState.hideCurrentSnackBar();
+            _scaffoldKey.currentState.showSnackBar(_showSnackBar(document));
+            _delete(document);
+          });
+        }
+        return Card(
+          elevation: 3,
+          semanticContainer: true,
+          child: CheckboxListTile(
+              title: Text(
+                '${document.data()['Title']}',
+                style: TextStyle(
+                    decoration: document.data()['isCheck']
+                        ? TextDecoration.lineThrough
+                        : TextDecoration.none),
+              ),
+              value: document.data()['isCheck'],
+              contentPadding: EdgeInsets.all(0),
+              controlAffinity: ListTileControlAffinity.leading,
+              onChanged: (value) => _collection.doc(document.id).update(
+                  {'Title': document.data()['Title'], 'isCheck': value})),
+          color: Theme.of(context).cardColor,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        );
+      }).toList(),
+    );
   }
 }
